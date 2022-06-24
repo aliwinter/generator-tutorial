6e2e35d297136094acda880791bc5056c459f5ac
Ali Akram <aliakram@winterlightlabs.com>
2022-06-23 23:14:34 -0400

    first commit

---
+++ gen_array.py
@@ -0,0 +1,37 @@
+import time
+from venv import create
+
+
+def csv_reader_not_generator(file_name):
+    file = open(file_name)  # a generator is returned here
+    result = file.read().split("\n")  # return a LIST of results
+    file.close()
+    return result
+
+
+def create_new_array_no_generator(arr):
+    new_arr = []
+    for row in arr:
+        new_arr = row + "NEW LINE"
+    return new_arr
+
+
+def create_new_array_generator(arr):
+    for row in arr:
+        yield row + "NEW LINE"
+
+
+# We want to iterate  this large list of rows in our file
+arr = csv_reader_not_generator("big_file.txt")
+
+# GOAL: Create a new array with with the values from previous array
+
+print("Using generator...")
+start_time = time.time()
+new_arr = create_new_array_generator(arr)
+print("TOTAL_TIME:", time.time() - start_time)
+
+print("\nNot using generator...")
+start_time = time.time()
+new_arr = create_new_array_no_generator(arr)
+print("TOTAL_TIME:", time.time() - start_time)
---
+++ gen_open_file.py
@@ -0,0 +1,33 @@
+import time
+
+
+def csv_reader_not_generator(file_name):
+    file = open(file_name)  # a generator is returned here
+    result = file.read().split("\n")  # return a LIST of results
+    file.close()
+    return result
+
+
+def csv_reader_generator(file_name):
+    # open(file_name, "r") -> returns a generator
+    for row in open(file_name, "r"):
+        yield row  # returns a generator object
+
+
+print("Using generator...")
+start_time = time.time()
+csv_gen = csv_reader_generator("big_file.txt")
+row_count = 0
+for row in csv_gen:
+    row_count += 1
+print(f"Row count is {row_count}")
+print("TOTAL_TIME:", time.time() - start_time)
+
+print("\nNot using generator...")
+start_time = time.time()
+csv_gen = csv_reader_not_generator("big_file.txt")
+row_count = 0
+for row in csv_gen:
+    row_count += 1
+print(f"Row count is {row_count}")
+print("TOTAL_TIME:", time.time() - start_time)
